bc20675c3ef571b0bf679fa25acc28f75ebfc5e2
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-18 16:48:00 +0300

    Added new changes

--- person_class.rb
+++ person_class.rb
@@ -1,19 +1,23 @@
+# frozen_string_literal: true
+
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = nil
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    return false unless of_age? || parent_permission
+    of_age? || @parent_permission
   end
 
+  private
+
   def of_age?
-    return false unless @age >= 18
+    `@age >= 18`
   end
 end
