15d1103c3bdc552e882de487efb318a9de217091
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-19 09:10:25 +0300

    Added new files

---
+++ check_decorators.rb
@@ -0,0 +1,9 @@
+require './person_class'
+require './decorator'
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
--- nameable.rb
+++ nameable.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class Nameable
   def correct_name
-    raise NotImplementedError, "#{self.class} should adhere to decorators"
+    raise NotImplementedError, "#{self.class} must be implemented by the sub classes"
   end
 end
