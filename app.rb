53897ca8a64e39983ee2a8265ca05eea3bade54b
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-20 21:56:23 +0300

    Fix the linter errors and fixed the bugs

--- app.rb
+++ app.rb
@@ -3,95 +3,85 @@ require_relative './student'
 require_relative './teacher'
 require_relative './rental'
 
-$person_list = []
-$rentals_list = []
-$students_list = []
-$teachers_list = []
-$books_list = []
-
-def list_all_books
-  return unless defined?($books_list)
-
-  $books_list.each do |book|
-    puts "[Book] Title: #{book.title} Author: #{book.author}"
+class Library
+  def initialize
+    @person_list = []
+    @rentals_list = []
+    @books_list = []
   end
-end
 
-def list_all_people
-  return unless defined?($person_list)
-
-  $person_list.each do |person|
-    if defined?(person.specialization)
-      puts "[Teacher] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
-    else
-      puts "[Student] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
+  def list_all_books
+    @books_list.each do |book|
+      puts "[Book] Title: #{book.title} Author: #{book.author}"
     end
   end
-end
 
-def create_student(age, name, parent_permission, classroom)
-  student = Student.new(age, name, parent_permission, classroom)
-  return unless student.is_a?(Person)
+  def list_all_people
+    @person_list.each do |person|
+      if person.is_a?(Teacher)
+        puts "[Teacher] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
+      else
+        puts "[Student] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
+      end
+    end
+  end
 
-  $person_list << student
-  puts "Student created successfully \n\n"
-end
+  def create_student(age, name, parent_permission, classroom)
+    student = Student.new(age, name, parent_permission, classroom)
+    if student.is_a?(Person)
+      @person_list << student
+      puts "Student created successfully \n\n"
+    end
+  end
 
-def create_teacher(age, name, specialization)
-  teacher = Teacher.new(age, name, nil, specialization)
-  return unless teacher.is_a?(Person)
+  def create_teacher(age, name, specialization)
+    teacher = Teacher.new(age, name, nil, specialization)
+    if teacher.is_a?(Person)
+      @person_list << teacher
+      puts "Teacher created successfully \n\n"
+    end
+  end
 
-  $person_list << teacher
-  puts "Teacher created successfully \n\n"
-end
+  def create_a_book(title, author)
+    book = Book.new(title, author)
+    if book.is_a?(Book)
+      @books_list << book
+      puts 'Book created successfully'
+    end
+  end
 
-def create_a_book(title, author)
-  books = Book.new(title, author)
+  def create_rental(date, person, book)
+    rental = Rental.new(date, person, book)
+    if rental.is_a?(Rental)
+      @rentals_list << rental
+      puts 'Rental created successfully'
+    end
+  end
 
-  return unless books.is_a?(Book)
-
-  $books_list << books
-  puts 'Book created sucessfully'
-end
-
-def create_rental(date, person, book)
-  rental = Rental.new(date, person, book)
-
-  return unless rental.is_a?(Rental)
-
-  $rentals_list << rental
-
-  puts 'Rentals created successfully'
-end
-
-def list_all_rentals(id)
-  $person_list.each do |person|
-    if person.id == id
+  def list_all_rentals(id)
+    person = @person_list.find { |p| p.id == id }
+    if person
       person.rentals.each do |rental|
         puts "[Rental] Date: #{rental.date}, Title: #{rental.book.title} Author: #{rental.book.author}"
       end
     else
-      puts 'No rental for this student.'
+      puts 'No rentals for this student.'
     end
   end
-end
 
-def exit_action
-  exit
-end
+  def exit_action
+    exit
+  end
 
-def choose_option(option)
-  all_options = {
-    1 => :list_all_books,
-    2 => :list_all_people,
-    3 => :create_a_person,
-    4 => :create_a_book,
-    5 => :create_a_rental,
-    6 => :list_all_rentals,
-    7 => :exit_action
-  }
-
-  all_options.each do |op, value|
-    send(value) if op == option.to_i
+  def choose_option(option)
+    case option.to_i
+    when 1 then list_all_books
+    when 2 then list_all_people
+    when 3 then create_a_person
+    when 4 then create_a_book
+    when 5 then create_a_rental
+    when 6 then list_all_rentals
+    when 7 then exit_action
+    end
   end
 end
