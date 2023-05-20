9da9f6312e033c82b21d140c25fed4bced9483ae
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-20 22:19:03 +0300

    Fix the linter errors and fixed the bugs

--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
 require_relative './app'
-require './app'
 
-# * Select 4
 def create_a_person
   puts 'Do you want to create (1) a Student or (2) a Teacher?'
   selected = gets.chomp.to_i
@@ -31,28 +29,24 @@ def create_a_book
   main
 end
 
-# * Create a Rental
-
 def create_a_rental
-  puts "Select the person form the following list by number (not id)\n\n"
-  if defined?($person_list)
-    $person_list.each_with_index do |person, index|
-      if defined?(person.specialization)
-        puts "[Teacher] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
-      else
-        puts "#{index}. [Student] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
-      end
+  puts "Select the person from the following list by number (not id)\n\n"
+  @person_list.each_with_index do |person, index|
+    if person.is_a?(Teacher)
+      puts "[Teacher] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
+    else
+      puts "#{index}. [Student] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
     end
   end
   selected_person = gets.chomp.to_i
   puts "Select the book from the following list\n\n"
-  $books_list.each_with_index do |book, index|
+  @books_list.each_with_index do |book, index|
     puts "#{index}. [Book] Name: #{book.title} Author: #{book.author}"
   end
   selected_book = gets.chomp.to_i
   puts 'Date'
   date = gets.chomp
-  create_rental(date, $person_list[selected_person], $books_list[selected_book])
+  create_rental(date, @person_list[selected_person], @books_list[selected_book])
 end
 
 def list_all_rentals
@@ -60,12 +54,11 @@ def list_all_rentals
   rental_id = gets.chomp.to_i
   list_rentals(rental_id)
 end
-# list_rentals(rental_id)
-# * The initialization part of the program
 
 puts 'Welcome to the School Library App'
+
 def main
-  puts "Please chose an option by entering an option\n"
+  puts "Please choose an option by entering an option\n"
   puts '1. List all Books'
   puts '2. List all People'
   puts '3. Create a Person'
